b52bcc26aad81ff81f97bd4b62b935b4b4cc6ba2
Max Hird <max@hirds.co.uk>
2019-03-02 08:51:25 +0000

    Delete 46 GolbachsOtherConjecture.py

--- 46 GolbachsOtherConjecture.py
+++
@@ -1,8 +0,0 @@
-# What is the smallest odd composite that cannot be written
-# as the sum of a prime and twice a square?
-
-def FindGoldbachsOtherConjecture():
-	return 0
-
-FindGoldbachsOtherConjecture()
-# Returns ????
