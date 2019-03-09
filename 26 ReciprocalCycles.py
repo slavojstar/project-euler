b25cadab4d74dcab1dd16423161f49ee3931d379
Max Hird <max@hirds.co.uk>
2019-03-09 17:17:24 +0000

    Update 26 ReciprocalCycles.py

--- 26 ReciprocalCycles.py
+++ 26 ReciprocalCycles.py
@@ -1,12 +1,31 @@
-# Find the value of d < 1000 for which 1/d contains the longest recurring cycle in its decimal fraction part.
-
-import math
+# Find the value of d < 1000 for which 1/d contains the longest
+# recurring cycle in its decimal fraction part.
 
 def FindReciprocalCycles():
 
-	
+	seqLength = 0
+	num = 0
 
-	return 0
+	for i in range(1000, 1, -1):
+		if seqLength >= i:
+			break
+
+		remainders = []
+		value = 1
+		position = 0
+
+		while remainders[value] == 0 and value != 0:
+			remainders[value] = position
+			value *= 10
+			value = value % i
+			position += 1
+
+		if position - remainders[value] > seqLength:
+			num = i
+			seqLength = position - remainders[value]
+
+	print(num)
+	return num
 
 FindReciprocalCycles()
-# Returns ?????
+# Returns 983
